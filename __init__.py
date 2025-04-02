97cd216d33d42c01d04a3a23162c45f7b641b02d
robieli <robieli@umich.edu>
2025-04-01 20:47:13 -0400

    Added Arabic support and updated live translations

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
 from langchain_core.messages import HumanMessage, SystemMessage
+from deep_translator import GoogleTranslator
 import model
 import aws
 
@@ -18,24 +19,34 @@ def get_translations(lang_code):
         # with open(f'translations/{lang_code}.json', 'r', encoding='utf-8') as file:
         #     translations = json.load(file)
         response = aws.get_json_from_s3("clues-languages", f"{lang_code}.json")
-        print (response)
         return jsonify(response)
     except FileNotFoundError:
         return jsonify({"error": "Language not found"}), 404
     
 @app.route('/llm')
 def get_llm():
-    lang = request.args.get('lang')
-    query = request.args.get('query')
-    llm = model.llm
-    messages = [
-        SystemMessage("""You should perform tasks like normal, except for that all of your responses 
-                      should be in the language that the user asks for. If the user does not ask for a
-                      specific language, you should answer in the language of their query. Please do not 
-                      use any markup notation, as your response will be displayed in plain text."""),
-        HumanMessage(query)
-    ]
-    return jsonify(llm.invoke(messages).content)
+    try:
+        query = request.args.get('query')
+        llm = model.llm
+        messages = [
+            SystemMessage("""You should perform tasks like normal, except for that all of your responses 
+                        should be in the language that the user asks for. If the user does not ask for a
+                        specific language, you should answer in the language of their query. Please do not 
+                        use any markup notation, as your response will be displayed in plain text."""),
+            HumanMessage(query)
+        ]
+        return jsonify(llm.invoke(messages).content)
+    except:
+        return jsonify({"error": "An error occurred"}), 404
+
+@app.route('/translate')
+def get_live_translation():
+    try:
+        translator= GoogleTranslator(target=request.args.get('lang'), source='auto')
+        translation = translator.translate(request.args.get('query'))
+        return jsonify(translation)
+    except:
+        return jsonify({"error": "An error occurred"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
